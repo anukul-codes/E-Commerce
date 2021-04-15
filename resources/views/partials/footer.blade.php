c9fda1c63533cb7a47b0b6499e82b1bd9d38c583
anukul-codes <82258347+anukul-codes@users.noreply.github.com>
2021-04-15 09:47:38 +0530

    Add files via upload

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="footer-content container">
+        <div class="made-with">Made by Mohd Amaan Khan </div>
+        <div class="made-with">Registration number=>11805050</div>
+        {{ menu('footer', 'partials.menus.footer') }}
+    </div> <!-- end footer-content -->
+</footer>
