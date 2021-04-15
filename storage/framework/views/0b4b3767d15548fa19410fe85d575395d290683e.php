ad91cfab65082522149943607b1b3b29e29ef9b4
anukul-codes <82258347+anukul-codes@users.noreply.github.com>
2021-04-15 09:40:41 +0530

    Add files via upload

---
+++ storage/framework/views/0b4b3767d15548fa19410fe85d575395d290683e.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="footer-content container">
+        <div class="made-with">Made by Mohd Amaan Khan </div>
+        <div class="made-with">Registration number=>11805050</div>
+        <?php echo e(menu('footer', 'partials.menus.footer')); ?>
+
+    </div> <!-- end footer-content -->
+</footer>
+<?php /**PATH D:\pull\laravel-ecommerce-project\resources\views/partials/footer.blade.php ENDPATH**/ ?>
